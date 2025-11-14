eb19ddd75d3866f4e12dee01a4ad9537e0976020
kauan silas de jesus <kauan.silas.jesus@escola.pr.gov.br>
2025-11-14 14:34:53 +0000

    roleta

---
+++ script.js
@@ -0,0 +1,9 @@
+//Seleciona os elementos das luzes
+const luzVermelho = document.getElementById("vermelho");
+const luzAzul = document.getElementById("azul");
+const luzVerde = document.getElementById("verde");
+const luzAmarelo = document.getElementById("amarelo");
+
+// Botões
+const btnPara = document.getElementById("btnPara");
+const btnGirar = document.getElementById("btnGirar");
